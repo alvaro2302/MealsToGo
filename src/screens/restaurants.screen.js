d1b45b6b22c887dbf6b996b602f4fd2704f2a3fc
acuiza <alvaro230297@hotmail.com>
2023-06-25 15:35:55 -0400

    change screen for simulate more elements

--- src/screens/restaurants.screen.js
+++ src/screens/restaurants.screen.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { View, FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
 import styled from "styled-components";
 import { RestaurantInfoCard } from "../components/restaurantInfoCard.component";
 import { Spacer } from "../components/spacer/spacer.component";
 import { SafeArea } from "../components/utility/safe-area.component";
+import { Search } from "../components/search.component";
 
-const SearchContainer = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
+
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 export const RestaurantsScreen = () => (
   <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
+    <Search/>
     <RestaurantList
       data={[
         { name: 1 },
