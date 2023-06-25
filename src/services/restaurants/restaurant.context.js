27694a9a40ec07c9ee85f8d31a10a35c76e63766
acuiza <alvaro230297@hotmail.com>
2023-06-25 16:46:12 -0400

    add context for restaurants

---
+++ src/services/restaurants/restaurant.context.js
@@ -0,0 +1,13 @@
+import React, {useState,useEffect,useMemo, createContext,} from "react";
+
+import { restaurantsRequest,restaurantsTransform } from "./restaurants.service";
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = ({children}) => {
+    return (
+            <RestaurantsContext.Provider value= {{restaurants:[1,2,3]}}>  
+                {children} 
+            </RestaurantsContext.Provider>
+        );
+};
