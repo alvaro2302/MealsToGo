a8796f3723af50a2340351fe8fa9e0b92d54c357
acuiza <alvaro230297@hotmail.com>
2023-06-26 14:16:32 -0400

    change values in context and validate in service this loading and error

--- src/services/restaurants/restaurant.context.js
+++ src/services/restaurants/restaurant.context.js
@@ -5,8 +5,30 @@ import { restaurantsRequest,restaurantsTransform } from "./restaurants.service";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({children}) => {
+    const [restaurants,setRestaurants] = useState([]);
+    const [isLoading,setIsloading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const retrieveRestaurants = () =>{
+        setIsloading(true);
+        setTimeout(() =>{
+            restaurantsRequest()
+            .then(restaurantsTransform)
+            .then((results)=>{
+                setIsloading(false);
+                setRestaurants(results);
+            })
+            .catch((error) => {
+                setIsloading(false);
+                setError(error);
+            })
+        },2000)
+    }
+    useEffect(() =>{
+        retrieveRestaurants();
+    }, [])
     return (
-            <RestaurantsContext.Provider value= {{restaurants:[1,2,3]}}>  
+            <RestaurantsContext.Provider value= {{restaurants, isLoading,error}}>  
                 {children} 
             </RestaurantsContext.Provider>
         );
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,5 +1,6 @@
 import { mocks } from "./mock/index.js";
 import camelize from "camelize";
+import { mockImages } from "./mock/index.js";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     return new Promise((resolve, reject) => {
         const mock = mocks[location];
@@ -14,11 +15,15 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
 export const restaurantsTransform = ({ results = []} )=> {
 
     const mappedResults = results.map((restaurant) => {
+        restaurant.photos = restaurant.photos.map(()=>{
+            return mockImages[Math.ceil(Math.random() * (mockImages.length -1))];
+        })
         return {
             ...restaurant,
             isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
             isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY"
         }
+        
     })
     return camelize(mappedResults);
 }
