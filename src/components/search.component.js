99cfbecd3c274389af57220f176fd19b462ef25e
acuiza <alvaro230297@hotmail.com>
2023-07-06 23:14:57 -0400

    change context in search component

--- src/components/search.component.js
+++ src/components/search.component.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React,{useContext, useState} from "react";
 import { View } from "react-native";
 import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
-
+import { LocationContext } from "../services/location/location.context";
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 export const Search = () => {
+    const {search,keyword} = useContext(LocationContext);
+    const [searchTerm,setSearchTerm] = useState(keyword);
     return (
         <SearchContainer>
             <Searchbar 
                 placeholder="search a restaurant"
+                value={searchTerm}
+                onChangeText={setSearchTerm}
+                onSubmitEditing={()=> search(searchTerm)}
             />
         </SearchContainer>
     )
