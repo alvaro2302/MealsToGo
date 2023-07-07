e2ab529ae2c884e84015a6ad117240b562ff2ca0
acuiza <alvaro230297@hotmail.com>
2023-07-06 23:18:56 -0400

    change location to default for parameter location

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,7 @@
 import { mocks } from "./mock/index.js";
 import camelize from "camelize";
 import { mockImages } from "./mock/index.js";
-export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+export const restaurantsRequest = (location) => {
     return new Promise((resolve, reject) => {
         const mock = mocks[location];
         if (!mock)
