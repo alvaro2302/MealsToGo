7cd8f320f82f9d5e0e793b857d61e2b2c19fa783
acuiza <alvaro230297@hotmail.com>
2023-07-06 23:15:58 -0400

    add logic when loading in the search or loading restaurants

--- src/screens/restaurants.screen.js
+++ src/screens/restaurants.screen.js
@@ -1,30 +1,49 @@
-import React, {useContext} from "react";
+import React, {useCallback, useContext} from "react";
 import { View, FlatList } from "react-native";
 import styled from "styled-components";
 import { RestaurantInfoCard } from "../components/restaurantInfoCard.component";
 import { Spacer } from "../components/spacer/spacer.component";
 import { SafeArea } from "../components/utility/safe-area.component";
 import { Search } from "../components/search.component";
-
+import { ActivityIndicator } from "react-native-paper";
 import { RestaurantsContext } from "../services/restaurants/restaurant.context";
-
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 
+const LoadingSpinner = styled(ActivityIndicator)`
+  margin-left:-25;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
-  const {restaurants,isLoading,error} = useContext(RestaurantsContext)
-  return (<SafeArea>
-    <Search/>
-    <RestaurantList
-      data={ restaurants}
-      renderItem={( {item, index}) => (
-        <Spacer position="bottom" size="large"  >
-          <RestaurantInfoCard restaurant= {item} uniqued= {index}/>
-        </Spacer>
-      )}
-      keyExtractor={(item) => item.name}
-    />
-  </SafeArea>
+  const {restaurants,isLoading,error} = useContext(RestaurantsContext);
+
+  return (
+
+    <SafeArea>
+      
+      
+      {isLoading && (
+        <LoadingContainer>
+          <LoadingSpinner animating={isLoading} color={"red"} size={"large"}/>
+        </LoadingContainer>
+        )
+      }
+       <Search/>
+        <RestaurantList
+          data={ restaurants}
+          renderItem={( {item, index}) => (
+            <Spacer position="bottom" size="large"  >
+              <RestaurantInfoCard restaurant= {item} uniqued= {index}/>
+            </Spacer>
+          )}
+          keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
   );
 }
